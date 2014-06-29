9f5711db4c51f3e3d44bdab8f6ec50fbd3e3f2d8
Brooks Travis <brooks.travis@gmail.com>
2014-06-29 01:15:00 -0500

    Adding the option to store MAC addresses as strings in the databas.

--- macaddress/fields.py
+++ macaddress/fields.py
@@ -1,34 +1,49 @@
 from django.core.exceptions import ValidationError
 from django.db import models
 
-from netaddr import EUI, AddrFormatError, mac_eui48
+from netaddr import EUI, AddrFormatError
 
-from formfields import MACAddressField as MACAddressFormField
+from .formfields import MACAddressField as MACAddressFormField
 
-
+from . import default_dialect, format_mac
+                 
 class MACAddressField(models.Field):
     description = "A MAC address validated by netaddr.EUI"
     empty_strings_allowed = False
     __metaclass__ = models.SubfieldBase
-
+    
+    def __init__(self, integer=True, *args, **kwargs): # Custom __init__ to accept new "integer" boolean argument to support specification of string or integer storage. Defaults to True (for now).
+        self.integer = integer
+        if not self.integer: # If storing MAC address as string, set max_length to default (17) or use supplied kwarg value.
+            kwargs['max_length'] = kwargs.get('max_length', 17)
+        super(MACAddressField, self).__init__(*args, **kwargs)
+    
     def get_prep_value(self, value):
         if value is None:
             return None
         if not isinstance(value, EUI):
-            return int(EUI(value, dialect=mac_eui48))
-        return int(value)
-
+            value = EUI(value, dialect=default_dialect())
+            if self.integer:
+                return int(value)
+            return str(value)
+        value.dialect = default_dialect(self)
+        if self.integer:
+            return int(value)
+        return str(value)
+    
     def get_internal_type(self):
-        return "BigIntegerField"
-
+        if self.integer:
+            return 'BigIntegerField'
+        return 'CharField'
+            
     def to_python(self, value):
         if value is None:
             return value
         if isinstance(value, EUI):
-            value.dialect = mac_eui48
+            value.dialect = default_dialect(value)
             return value
         try:
-            return EUI(value, dialect=mac_eui48)
+            return EUI(value, dialect=default_dialect())
         except (TypeError, ValueError, AddrFormatError):
             raise ValidationError(
                 "This value must be a valid MAC address.")
